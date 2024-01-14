aac4e5dc987d63be78885166a010716662ab1016
Thomas Rossenblum <149984771+tprossenblum@users.noreply.github.com>
2024-01-13 18:09:36 -0800

    Add files via upload

--- sports_ref_prompt.py
+++ sports_ref_prompt.py
@@ -9,24 +9,24 @@ teams = list(data.keys())
 
 # initialize the header and column string
 header = ' |---|'
-column = ''
-# loop through the teams and create header and team in column
+rows = ''
+# loop through the teams and create header and team in row
 # ^5 and ^3 are used for spacing
 for i in range(len(teams)):
     header = header + f'{teams[i]:^5}|'
-    column = column + f'\n |{teams[i]:^3}|'
+    rows = rows + f'\n |{teams[i]:^3}|'
     # nested loop to get results from the teams
     for j in range(len(teams)):
         # if on the same step we skip as it would be against own team
         if i == j:
-            column += " --- |"
+            rows += " --- |"
         # if valid data retrieve appropriate W column and print
         else:
-            column += f" {data[teams[i]][teams[j]]['W']:^3} |"
+            rows += f" {data[teams[i]][teams[j]]['W']:^3} |"
 
 # print header and columns to display data
 print(header, end='')
-print(column)
+print(rows)
 
 
 
